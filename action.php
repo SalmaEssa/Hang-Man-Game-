235e33e4b11b5f19fbda198453fa86a29fb3e7d1
Salma Essa Fouad <49573264+SalmaEssa@users.noreply.github.com>
2020-08-05 09:02:00 +0200

    Add files via upload

--- action.php
+++ action.php
@@ -1,103 +1,270 @@
-<?php require('head.php');
-include 'classes/class.php';
-
+<!DOCTYBE html>
+  
+<?php
+      include 'class.php';
+//session_start();
+//$_SESSION['guess']="";
+	  
+//class hangman
+//{
+//    public function get_ran_word($filename)
+//    {
+//
+//        $lines = array();
+//        $fp = fopen($filename, 'r');
+//        while (!feof($fp)) {
+//            $line = fgets($fp);
+//
+////process line however you like
+//            $line = trim($line);
+////https://www.w3schools.com/php/func_string_trim.asp
+//
+////add to array
+//            $lines[] = $line;
+//
+//        }
+//
+//        $key = array_rand($lines);
+//
+//        fclose($fp);
+//        return $lines[$key];
+//
+//
+//    }
+//
+//    public function read_file($filename)
+//    {
+//        $lines = array();
+//        $fp = fopen($filename, 'r');
+//        while (!feof($fp)) {
+//            $line = fgets($fp);
+//
+////process line however you like
+//            $line = trim($line);
+////https://www.w3schools.com/php/func_string_trim.asp
+//
+////add to array
+//            $lines[] = $line;
+//
+//        }
+//        return $lines;
+//    }
+//
+//    public function enter_user($username)
+//    {
+//        $userfile = 'users.txt';
+//// Open the file to get existing content
+//        $current = file_get_contents($userfile);
+//// Append a new person to the file
+//        $current .= $username;
+//// Write the contents back to the file
+//        file_put_contents($userfile, $current);
+//
+//    }
+//
+//    public function delete_user()
+//    {
+//        $myfile = "users.txt";
+//        $fh = fopen($myfile, 'w') or die ("can't open file");
+//        $stringdata = "";
+//        fwrite($fh, $stringdata);
+//        fclose($fh);
+//
+//    }
+//
+//    public function dashes($word)
+//    {
+//
+//        $len = strlen($word);
+//        for ($i = 0; $i < $len; $i++)
+//            $guesstemplate = str_repeat("-", $len);
+//        return $guesstemplate;
+//
+//    }
+//
+//    public function check_letter($word, $char, &$b, &$gess)
+//    {
+//        $ans = false;
+//        $len = strlen($word);
+//        $word = str_split($word);
+//        if (!isset($_SESSION['guess'])) {
+//            $_SESSION['guess'] = $gess;
+//        }
+//
+//        for ($i = 0; $i < $len; $i++) {
+//            if ($word[$i] == $char) {
+//                $gess[$i] = $char;
+//                $_SESSION['guess'][$i] = $char;
+//// echo $str[$i];
+//                $ans = true;
+//            }
+//        }
+////  =$str ;
+//        if ($ans == false) {
+//            $b = false;
+//        }
+//        return $_SESSION['guess'];
+//    }
+//
+//    public function hint($word, &$gess)
+//    {
+//        $len = strlen($word);
+//
+//        $word = str_split($word);
+//
+//        if (!isset($_SESSION['guess'])) {
+//            $_SESSION['guess'] = $gess;
+//        }
+//
+//        for ($i = 0; $i < $len; $i++) {
+//            if ($_SESSION['guess'][$i] == "-") {
+//                $var = $word[$i];
+//                for ($j = $i; $j < $len; $j++) {
+//                    if ($word[$j] == $var) {
+//                        $gess[$i] = $var;
+//                        $_SESSION['guess'][$j] = $var;
+//
+//                    }
+//                }
+//                break;
+//            }
+//        }
+//    }
+//
+//    public function check_wrong($char)
+//    {
+//		if(!isset($_SESSION['wrong_letters']))
+//			$_SESSION['wrong_letters']=[];
+//        if(!in_array($char , $_SESSION['wrong_letters']))
+//            $_SESSION['wrong_letters'][] = $char;
+//
+//        //print_r($_SESSION['wrong_letters']);
+//    }
+//    public function count_wrong()
+//    {
+//		if(!isset($_SESSION['wrong_letters']))
+//			$_SESSION['wrong_letters']=[];
+//        return count($_SESSION['wrong_letters']);
+//    }
+//
+//    public function draw_img($count)
+//    {
+//        if(!isset($count))
+//            $count=0;
+//        $imagearray=["hang_photos/hangman_0.gif","hang_photos/hangman_1.gif","hang_photos/hangman_2.gif","hang_photos/hangman_3.gif","hang_photos/hangman_4.gif","hang_photos/hangman_5.gif","hang_photos/hangman_6.gif","hang_photos/hangman_7.gif","hang_photos/hangman_8.gif","hang_photos/hangman_9.gif"];
+//        return $imagearray[$count];
+//    }
+//}
 ?>
 
-<?php
+
+
+<html>
+
+        <form method="post" action="<?php $_SERVER["PHP_SELF"]; ?>">
+
+        <input type="text" name="letter">
+    <!--	  <input type="submit" name="hint" value="Hint">-->
+         <input type="submit" name="submit">
+         </form>
+
+        <form method="post" action="<?php $_SERVER["PHP_SELF"]; ?>">
+         <input type="submit" name="hint" value="Hint">
+		 <input  type="submit" name="reset" value="reset"> 
+       </form>
+       <form method="post" action="Untitled-2.php" >
+            <input type="submit" name="pause" value="Pause">
+            
+       </form> 
+
+		<?php
 
 $obj= new hangman();
-$level=$_SESSION['level'];
-$category=$_SESSION['category'];
+
+$filename="words.txt";
 
 if(!isset($_SESSION['word'])) {
-    $_SESSION['word'] = $obj->user_word ($level,$category);
+    $_SESSION['word'] = $obj->get_ran_word($filename);
+//$_SESSION['word']=$word;
 }
+
 $word = $_SESSION['word'];
-//echo ($word);
+
+// $word="mohammed";
 $len =  strlen($word);
 
-$gess= $obj->dashes($word);
+$gess= $obj->dashes($word) ;
 $gess= str_split($gess);
-if(!isset($_SESSION['guess']))
-{
-	$_SESSION['guess'] = $gess;
-}
-$dashes_game = implode('  ', $_SESSION['guess']);
 $b=true;
-$lost =0;$won=0;
-$image = "images/hangman_0.gif";
-/*if(isset($_POST["resume"]))
-{
-	   $count_wrong = $obj->count_wrong();
-    $image = $obj->draw_img($count_wrong);
-    $dashes_game = implode(' ', $_SESSION['guess']);	
-}
-else
-{
-	
-
-	$image = "images/hangman_0.gif";
-}	*/
-//pause
+$lost =0;
+echo $word;
+echo "</br>";
+$image = "hang_photos/hangman_0.gif";
+	  
+	  
+	  
 if(isset($_POST["submit"])) {
-    $char = strtolower($_POST["letter"]);
+    $char = $_POST["letter"];
     $obj->check_letter($word, $char, $b, $gess);
+//print_r ($guss);
     if ($b == false) {
+        echo "wrong";
         $obj->check_wrong($char);
-        $dashes_game = implode('  ', $_SESSION['guess']);
-    }
-    else {
+        echo "<br>";
+        echo implode(' ', $_SESSION['guess']);
+    } 
+	else {
         for ($i = 0; $i < $len; $i++) {
             if ($i == $len - 1) {
-//echo implode($_SESSION['guess']); //implode(' ',$_SESSION['guess']);
-                $dashes_game = implode('  ', $_SESSION['guess']);
+//var_dump($_SESSION['guess']); //implode(' ',$_SESSION['guess']);
+                echo implode(' ', $_SESSION['guess']);
             }
         }
     }
-    $count_wrong = $obj->count_wrong();
-
+	$count_wrong = $obj->count_wrong();
+	
     $image = $obj->draw_img($count_wrong);
-    if($count_wrong==9)
-    {
-        $lost = 1;
-		header('location: page7_en.php');
-    }
-	$won = $obj->check_won();
-}
-else if(isset($_POST["hint"]))
-{
-	if($_SESSION['hint_flag']!=1){
-		$_SESSION['hint_flag'] = $obj->hint($word, $gess);
+	if($count_wrong==9)
+	{
+		$lost = 1;
+		$_SESSION = [];
 	}
-    $count_wrong = $obj->count_wrong();
-
+}
+else if(isset($_POST["hint"])) 
+{
+//echo "hello";
+/*if(isset($_SESSION['hint_flag']))
+{
+if($_SESSION['hint_flag']==1){
+	//echo "You are already used your hint!!";
+	//echo "<script>alert('You are already used your hint!!')</script>";
+}
+}
+	else*/
+	
+		//$_SESSION['hint_flag']=1;
+	
+    $obj->hint($word, $gess);
+		$count_wrong = $obj->count_wrong();
+	
     $image = $obj->draw_img($count_wrong);
-    $dashes_game = implode(' ', $_SESSION['guess']);
+    echo implode(' ', $_SESSION['guess']);
 	
-	$won = $obj->check_won();
 }
-else if(isset($_POST["reset"])) {
-
-    //unset($_SESSION);
-    $username = $_SESSION['username'];
-    $category = $_SESSION['category'];
-    $level = $_SESSION['level'];
-    $_SESSION = [];
-	
-	$_SESSION['username'] = $username;
-    $_SESSION['category'] = $category;
-    $_SESSION['level'] = $level;
-    $_SESSION['hint_flag'] = 0;
-	
-    header('Location: '.$_SERVER['REQUEST_URI']);
-
+	else if(isset($_POST["reset"])) {
+		
+        //unset($_SESSION);
+		$_SESSION = [];
+		 header('Location: '.$_SERVER['REQUEST_URI']);
+		
 }
-if($won==1)
-	header('location: page6_en.php');
-
+/*else {
+//Delete All Guesses Save In Session
+     $_SESSION = [];
+}*/
 ?>
 
-<?php require_once('the_form.php');?>
- 
-
-<?php require_once('footer.php');?>
+    <div class="hang"><img src="<?php echo $image;?>" id="mainimage" width= 350 height=350></div>
+	<?php if($lost==1) echo 'You lost';?>
+		
+    </html>
